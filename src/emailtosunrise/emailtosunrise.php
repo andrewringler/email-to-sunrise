511c267ddf5c07aa21c989217bb878d164ffde2a
Andrew Ringler <ringler@gmail.com>
2013-01-27 17:27:45 -0500

    cleanup

--- src/emailtosunrise/emailtosunrise.php
+++ src/emailtosunrise/emailtosunrise.php
@@ -9,6 +9,7 @@ Plugin URI: https://github.com/andrewringler/email-to-sunrise
 Description: My family sends around sunrise and sunset photos via email,
 this wordpress plugin parses those emails and converts them into Wordress posts maintaining 
 threaded conversations by converting all replies to comments on the original post.
+Adapted from wordpress core wp-mail.php and http://wordpress.org/extend/plugins/postie/
 Author: Andrew Ringler
 Version: 0.1
 Author URI: http://andrewringler.com/
@@ -16,18 +17,23 @@ Author URI: http://andrewringler.com/
 require 'vendor/autoload.php';
 require 'database.php';
 require 'email.php';
+require 'posting.php';
 
 
 register_activation_hook(__FILE__,'email_to_sunrise_install_db');
 // TODO add upgrade function http://codex.wordpress.org/Creating_Tables_with_Plugins
 
-/*
-Runs upon visiting the url: yoursite.com/wp-mail.php
-
-TODO the hook doesn't really allow for circumventing the email checking process
-it would probably better to use URL rewrites to register a service URL
-as something like http://yoursite.com/email-to-sunrise
-*/
+function email_to_sunrise_post() {
+  check_email_populate_db();
+  create_post_and_comments_from_db(); 
+  
+  // TODO the hook doesn't really allow for circumventing the email checking process
+  // so we have to kill the script here.
+  // it would probably better to use URL rewrites to register a service URL
+  // as something like http://yoursite.com/email-to-sunrise
+  wp_die( __( 'Ok' ), 'Ok' );
+}
+//Runs upon visiting the url: yoursite.com/wp-mail.php
 add_action('wp-mail.php', 'email_to_sunrise_post');
 
 ?>
--- src/emailtosunrise/posting.php
+++ src/emailtosunrise/posting.php
@@ -1,5 +1,81 @@
 <?php
 
+require_once(ABSPATH . 'wp-admin/includes/image.php');
+require_once(ABSPATH . 'wp-admin/includes/file.php');
 
+function create_post_and_comments_from_db() {
+  // create posts from database
+  $new_posts = new_posts();
+  foreach ($new_posts as $post) {
+   $userdata = get_user_by('email', $post->author);
+   if ( ! empty( $userdata ) ) {
+     $author_id = $userdata->ID;
+     
+     $post_insert = array(
+         'post_title'    => $post->subject,
+         'post_content'  => $post->body,
+         'post_status'   => 'publish',
+         'post_author'   => $author_id
+      );
+      $post_id = wp_insert_post( $post_insert );
+      add_post_meta( $post_id, 'emailtosunrise_email_id', $post->id );
+      
+      $image = get_image($post->id);
+      echo '<p>image: '. esc_html(var_export($image, true)) ."</p>\n";
+      $wp_filetype = wp_check_filetype(basename($image->filename), null);
+      $attachment = array(
+         'guid' => $image->image_url, 
+         'post_mime_type' => $wp_filetype['type'],
+         'post_title' => preg_replace('/\.[^.]+$/', '', basename($image->filename)),
+         'post_content' => '',
+         'post_status' => 'inherit'
+      );
+      $attach_id = wp_insert_attachment( $attachment, $image->filename, $post_id );
+      echo '<p>attach id: '. esc_html(var_export($attach_id, true)) ."</p>\n";
+      $attach_data = wp_generate_attachment_metadata( $attach_id, $image->filename );
+      echo '<p>attach data: '. esc_html(var_export($attach_data, true)) ."</p>\n";
+      if ( $attach_data ) {
+        wp_update_attachment_metadata( $attach_id, $attach_data );         
+        
+        // pre-pend image to post body
+        $image_post = wp_get_attachment_image( $attach_id, 'full' );
+        
+        $new_post_body = $image_post ."\n". $post->body;
+        wp_update_post( array( 'ID' => $post_id, 'post_content' => $new_post_body) );
+        
+        // <a href="http://192.168.33.20/?attachment_id=1140" rel="attachment wp-att-1140">
+        //   <img class="alignnone size-full wp-image-1140" alt="emailtosunrisezUyM1H" src="http://192.168.33.20/wp-content/uploads/2013/01/emailtosunrisezUyM1H.jpg" width="640" height="640" />
+        //  </a>
+      }
+      
+      update_message_status( $post->id, 'posted' );
+   }		
+  }
+   
+  // // create comments from database
+  // $new_comments = new_comments();
+  // foreach ($new_comments as $comment) {
+  //   $time = current_time('mysql');
+  //   
+  //   $post_id = wordpress_post_id_for_email_id( $comment )
+  //   
+  //   $data = array(
+  //       'comment_post_ID' => 1,
+  //       'comment_author' => 'admin',
+  //       'comment_author_email' => 'admin@admin.com',
+  //       'comment_author_url' => 'http://',
+  //       'comment_content' => 'content here',
+  //       'comment_type' => '',
+  //       'comment_parent' => 0,
+  //       'user_id' => 1,
+  //       'comment_author_IP' => '127.0.0.1',
+  //       'comment_agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.10) Gecko/2009042316 Firefox/3.0.10 (.NET CLR 3.5.30729)',
+  //       'comment_date' => $time,
+  //       'comment_approved' => 1,
+  //   );
+  // 
+  //   wp_insert_comment($data);
+  // }  
+}
 
 ?>
